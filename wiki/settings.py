969d44cb4af804243635d702e744246f59fe370b
richardGonzalez-std <84289391+richardGonzalez-std@users.noreply.github.com>
2025-09-04 16:23:38 -0400

    Update settings.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://wikientries.onrender.com"
+]
 
 
 # Application definition
